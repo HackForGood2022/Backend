ed2e30ef901350966423044f39e38ede88e3e38d
Jorg403 <76389552+Jorg403@users.noreply.github.com>
2022-10-21 18:37:14 +0200

    joum

--- pages/api/general/home.js
+++ pages/api/general/home.js
@@ -2,15 +2,15 @@
 export default async (req, res) => {
 	let ofertas = [
 		// top 4 friends with the higest score
-		{ offer: "Ganadero/a", village: "Villanueva de Sigena", name: "Ayelen Nuño", username: "@ayelen", picture: "https://www.publico.es/uploads/2017/11/16/5a0d6cbbd22d5.jpg" },
-		{ offer: "Panadero/a", village: "La Puebla de Hijar", name: "Ismael Tienda", username: "@Ismati5", picture: "https://www.turismodearagon.com/wp-content/uploads/2014/04/la_puebla_de_hijar_-_bajo_martin.jpg" },
-		{ offer: "Librero/a", village: "Hoz de Barbastro", name: "Jaime Roncal", username: "@jaimeoncal_", picture: "https://www.turismodearagon.com/wp-content/uploads/2014/04/hoz_de_barbastro_-_somontano_de_barbastro_-_foto_jose_ma_puig_1.jpg" },
-		{ offer: "Administrativo", village: "Canfranc", name: "Raúl", username: "@raulIS", picture: "https://www.publico.es/uploads/2017/11/16/5a0d6cbbd22d5.jpg" },
-		{ offer: "Carnicero/a", village: "ALcolea", name: "Jaime Roncal", username: "@jaimeroncal_", picture: "https://www.publico.es/uploads/2017/11/16/5a0d6cbbd22d5.jpg" },
-		{ offer: "Paisajista", village: "Villanueva de Sigena", name: "Ayelen Nuño", username: "@ayelen2", picture: "https://www.publico.es/uploads/2017/11/16/5a0d6cbbd22d5.jpg" },
-		{ offer: "Temporeros", village: "Canfranc", name: "Jorge Aranda", username: "@j_jorge", picture: "https://blog.infoempleo.com/media/2017/07/ThinkstockPhotos-dv175003.jpg" },
-		{ offer: "Ingeniero/a forestal", village: "Fago", name: "Celia", username: "@celiaHw", picture: "https://media.istockphoto.com/photos/man-holding-paper-project-in-the-park-picture-id1167514305?k=20&m=1167514305&s=612x612&w=0&h=WmOsmlujf_Q9CBHUANokTc4FTRP_LtjE7H1W1pwDWZE=" },
-		{ offer: "Auxiliar de veterinaria", village: "La Puebla de Hijar", name: "Ruth López", username: "@ruth", picture: "https://elguadarramista.files.wordpress.com/2015/11/sin-tc3adtulo2.png" },
+		{ offer: "Ganadero/a", village: "Villanueva de Sigena", name: "Ayelen Nuño", username: "@ayelen", picture: 2},
+		{ offer: "Panadero/a", village: "La Puebla de Hijar", name: "Ismael Tienda", username: "@Ismati5", picture: 1},
+		{ offer: "Temporeros", village: "Canfranc", name: "Jorge Aranda", username: "@j_jorge", picture: 3},
+		{ offer: "Ingeniero/a forestal", village: "Fago", name: "Celia", username: "@celiaHw", picture: 4},
+		{ offer: "Librero/a", village: "Hoz de Barbastro", name: "Jaime Roncal", username: "@jaimeoncal_", picture: 2},
+		{ offer: "Administrativo", village: "Canfranc", name: "Raúl", username: "@raulIS", picture: 1},
+		{ offer: "Auxiliar de veterinaria", village: "La Puebla de Hijar", name: "Ruth López", username: "@ruth", picture: 5},
+		{ offer: "Carnicero/a", village: "ALcolea", name: "Jaime Roncal", username: "@jaimeroncal_", picture: 2},
+		{ offer: "Paisajista", village: "Villanueva de Sigena", name: "Ayelen Nuño", username: "@ayelen2", picture: 1},
 	];
 	res.status(200).json({
 		result: "success",
