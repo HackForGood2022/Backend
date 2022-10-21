9687a76df6acd54161aea9771420fce6f8dd19e1
Jorg403 <76389552+Jorg403@users.noreply.github.com>
2022-10-21 15:08:52 +0200

    w

--- prisma/queries/CREATE/quick_game.js
+++
@@ -1,8 +0,0 @@
-import prisma from "../../../lib/prisma";
-
-export async function createQuickGameDB(data) {
-	const query = await prisma.Quick_match.create({
-		data: data,
-	});
-	return query;
-}
